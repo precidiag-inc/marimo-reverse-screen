45bf39105aa4871e05f7b86a55dd69a0840b2349
varoth <varoth.lilas@gmail.com>
2025-01-30 22:04:40 +0700

    updated script location and logo

--- scripts/build.py
+++ scripts/build.py
@@ -59,7 +59,7 @@ def generate_index(all_notebooks: List[str], output_dir: str) -> None:
   </head>
   <body class="font-sans max-w-2xl mx-auto p-8 leading-relaxed">
     <div class="mb-8">
-      <img src="https://raw.githubusercontent.com/marimo-team/marimo/main/docs/_static/marimo-logotype-thick.svg" alt="marimo" class="h-20" />
+      <img src="https://www.einpresswire.com/image/large/815714/piton-tx.png" alt="marimo" class="h-20" />
     </div>
     <div class="grid gap-4">
 """
